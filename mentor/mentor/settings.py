d8deffd59a066eaec60012a39081b5d888896569
Joshua Levy <joshualevy@fb.com>
2021-03-31 21:37:34 -0700

    enable heroku 01

--- mentor/mentor/settings.py
+++ mentor/mentor/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = 'e9n)i!zp6o!5yl)kdzh6jk)#4)oxky6qy4igtakswev2+nf2fc'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1','https://mentorshipvictor.herokuapp.com/']
 
 
 # Application definition
