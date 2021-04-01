3268adc2038583911672ce5c5a4be91b78b42907
Joshua Levy <joshualevy@fb.com>
2021-03-31 21:25:22 -0700

    working forms

--- mentor/notes/forms.py
+++ mentor/notes/forms.py
@@ -1,7 +1,7 @@
 from django import forms
-from .models import Note
+from .models import Notes
 
 class NoteForm(forms.ModelForm):
-
     class Meta:
-        model = Note
+        model = Notes
+        fields = '__all__'
--- mentor/notes/views.py
+++ mentor/notes/views.py
@@ -2,13 +2,18 @@ from django.shortcuts import render
 from django.template import RequestContext, loader
 from django.http import HttpResponse
 from .models import Notes
+from .forms import NoteForm
 
 # Create your views here.
 
 def home(request):
     notes_obj = Notes.objects.all()
     template = loader.get_template('notes.html')
-    context = {'notes_obj': notes_obj}
+    form = NoteForm(request.POST or None)
+    if form.is_valid():
+        save_it = form.save(commit=False)
+        save_it.save()
 
+    context = {'notes_obj':notes_obj, 'form':form}
     return render(request, 'notes.html', context)
     #return render_to_response("note.html", notes)
